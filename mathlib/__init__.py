6208e23d747aed8b61824fb62961af9702c6e0a1
Yasser <yasserfedsi34@gmail.com>
2026-01-31 15:05:39 +0100

    Initial working mathlib package with tests

---
+++ mathlib/__init__.py
@@ -0,0 +1,3 @@
+from .basic import add, subtract, multiply, divide
+
+__all__ = ["add", "sbtract", "multiply", "divide"]
---
+++ mathlib/basic.py
@@ -0,0 +1,17 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+
+def divide(a: float, b: float) -> float:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        raise ValueError("Division by Zero is not allowed.")
